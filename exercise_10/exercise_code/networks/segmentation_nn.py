3ef83c7a3224fbb5427a22b5517be6b1d84bdea1
Alfred <alfrednguyen02@gmail.com>
2024-06-24 14:46:44 +0200

    added exercise 10

---
+++ exercise_10/exercise_code/networks/segmentation_nn.py
@@ -0,0 +1,78 @@
+"""SegmentationNN"""
+import torch
+import torch.nn as nn
+
+
+class SegmentationNN(nn.Module):
+
+    def __init__(self, num_classes=23, hp=None):
+        super().__init__()
+        self.hp = hp
+        #######################################################################
+        #                             YOUR CODE                               #
+        #######################################################################
+
+    
+        #######################################################################
+        #                           END OF YOUR CODE                          #
+        #######################################################################
+
+    def forward(self, x):
+        """
+        Forward pass of the convolutional neural network. Should not be called
+        manually but by calling a model instance directly.
+
+        Inputs:
+        - x: PyTorch input Variable
+        """
+        #######################################################################
+        #                             YOUR CODE                               #
+        #######################################################################
+
+        
+        #######################################################################
+        #                           END OF YOUR CODE                          #
+        #######################################################################
+
+        return x
+
+    # @property
+    # def is_cuda(self):
+    #     """
+    #     Check if model parameters are allocated on the GPU.
+    #     """
+    #     return next(self.parameters()).is_cuda
+
+    def save(self, path):
+        """
+        Save model with its parameters to the given path. Conventionally the
+        path should end with "*.model".
+
+        Inputs:
+        - path: path string
+        """
+        print('Saving model... %s' % path)
+        torch.save(self, path)
+
+        
+class DummySegmentationModel(nn.Module):
+
+    def __init__(self, target_image):
+        super().__init__()
+        def _to_one_hot(y, num_classes):
+            scatter_dim = len(y.size())
+            y_tensor = y.view(*y.size(), -1)
+            zeros = torch.zeros(*y.size(), num_classes, dtype=y.dtype)
+
+            return zeros.scatter(scatter_dim, y_tensor, 1)
+
+        target_image[target_image == -1] = 1
+
+        self.prediction = _to_one_hot(target_image, 23).permute(2, 0, 1).unsqueeze(0)
+
+    def forward(self, x):
+        return self.prediction.float()
+
+if __name__ == "__main__":
+    from torchinfo import summary
+    summary(SegmentationNN(), (1, 3, 240, 240), device="cpu")
